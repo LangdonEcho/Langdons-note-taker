5b2c9248df6fd12c9e93fbbeb507929f565dc42b
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-12-09 03:35:43 -0600

    creating routes

--- db/server.js
+++ db/server.js
@@ -1,9 +1,14 @@
 
-const mySQL = require("mysql");
+// Require Dependencies
 const express = require("express");
+const fs = require("fs");
+const path = require('path');
 
-var app = express();
-var PORT = 3001;
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
