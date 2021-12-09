3431f22aff5544a67021cfb37d788f10440353c7
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-12-09 04:01:59 -0600

    trying to fix the not taking saving

--- routes/routes.js
+++ routes/routes.js
@@ -1,36 +1,24 @@
-const fs = require('fs');
-const path = require('path');
+// Dependencies
+const router = require('express').Router();
 
-module.exports = app => {
+const saveData = require('../db/saveData');
 
-    // Setup notes variable
-    fs.readFile("db/db.json","utf8", (err, data) => {
+// GET request
+router.get('/notes', function (req, res) {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
 
-        if (err) throw err;
+// POST request
+router.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
 
-        var notes = JSON.parse(data);
 
-         // Setup the /api/notes get route
-         app.get("/api/notes", function(req, res) {
-            res.json(notes);
-        });
 
-        // Setup the /api/notes post route
-        app.post("/api/notes", function(req, res) {
-            let newNote = req.body;
-            notes.push(newNote);
-            updateDb();
-            return console.log("Added new note: "+newNote.title);
-        });
-
-        // Retrieves a note with specific id
-        app.get("/api/notes/:id", function(req,res) {
-            res.json(notes[req.params.id]);
-        });
-
-        // Deletes a note with specific id
-        app.delete("/api/notes/:id", function(req, res) {
-            notes.splice(req.params.id, 1);
-            updateDb();
-            console.log("Deleted note with id "+req.params.id);
-        })
+module.exports = router;
