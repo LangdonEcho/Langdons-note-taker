d78d94f4b470fff4c6a1bd6322cd478fcbc95b9a
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-12-09 03:52:13 -0600

    working on errors from routes

--- routes/routes.js
+++ routes/routes.js
@@ -9,3 +9,28 @@ module.exports = app => {
         if (err) throw err;
 
         var notes = JSON.parse(data);
+
+         // Setup the /api/notes get route
+         app.get("/api/notes", function(req, res) {
+            res.json(notes);
+        });
+
+        // Setup the /api/notes post route
+        app.post("/api/notes", function(req, res) {
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            return console.log("Added new note: "+newNote.title);
+        });
+
+        // Retrieves a note with specific id
+        app.get("/api/notes/:id", function(req,res) {
+            res.json(notes[req.params.id]);
+        });
+
+        // Deletes a note with specific id
+        app.delete("/api/notes/:id", function(req, res) {
+            notes.splice(req.params.id, 1);
+            updateDb();
+            console.log("Deleted note with id "+req.params.id);
+        })
